72a358b89a7f2b24b919bbbeff2b4fb101ac62bc
sosedov-official <sosedov.official@gmail.com>
2023-04-10 16:29:46 -0400

    BlockBuster v0.96

--- pages/success.js
+++ pages/success.js
@@ -1,11 +1,19 @@
 import { withPageAuthRequired } from "@auth0/nextjs-auth0";
 import { AppLayout } from "../components/AppLayout";
 import { getAppProps } from "../utils/getAppProps";
+import Link from "next/link";
 
 export default function Success() {
   return (
-    <div>
-      <h1>Thank you for your purchase!</h1>
+    <div className="text-center">
+      <form className='mx-auto my-56 w-full max-w-screen-sm bg-slate-100 p-4 rounded-md shadow-xl border border-slate-200 shadow-slate-200'>
+        <h1>
+          Thank you for your purchase!
+        </h1>
+        <Link href="/post/new" legacyBehavior>
+          <a className="btn mt-10">Generate a New Blog</a>
+        </Link>
+      </form>
     </div>
   );
 }
